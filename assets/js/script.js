16e9e001b739257174299a541a524ba9167ca55a
CleanOak <53487230+CleanOak@users.noreply.github.com>
2023-11-27 14:54:47 +0000

    ceate close button to clear list

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,9 @@ let itemInput = document.getElementById("myInput");
 let itemsList = document.getElementsByTagName("li");
 let i;
 
-/* */
-for (i = 0; i < itemsList.length; i++) {
-    let span = document.createElement("SPAN");
+/* Create close x button*/
+for (i = 0; i < itemsList.length; i++){
+    let span = document.createElement("span");
     let txt = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
