978a6337d2bc91ba814c0509cc28d51ba9c63151
CleanOak <53487230+CleanOak@users.noreply.github.com>
2023-12-01 14:20:21 +0000

    Align list and input

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,39 +6,4 @@ let span = document.createElement("span");
 let txt = document.createTextNode("\u00D7");
 let i;
 
-/* Create close x button*/
-for (i = 0; i < itemsList.length; i++){
-    span.className = "close";
-    span.appendChild(txt);
-    itemsList[i].appendChild(span);
-}
-
-for (i = 0; i < close.length; i++){
-
-}
-
-
-function newElement(){
-    let newItem = document.createElement("li");
-    let addToList = document.createTextNode(inputValue);
-
-    newItem.appendChild(addToList);
-    if (inputValue === " ") {
-        alert ("Type in something you want to do");
-    } else {
-        document.getElementById("myUL").appendChild(newItem);
-    }
-       document.getElementById("myInput").innerHTML = "";
-
-    span.className = "close";
-    span.appendChild(txt);
-    newItem.appendChild(span);
-
-    for (i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
-            let div = this.parentElement;
-            div.style.display = "none";
-        }
-    }
-}
 
