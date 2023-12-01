6a2771e622daca6267f367d8306ab9af6c2c7cc4
CleanOak <53487230+CleanOak@users.noreply.github.com>
2023-12-01 15:30:29 +0000

    Add media query

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,6 @@ let close = document.getElementsByClassName("close");
 let inputValue = document.getElementById("myinput");
 let span = document.createElement("span"); 
 let txt = document.createTextNode("\u00D7");
-let i;
+
 
 
