70ba10fb290db6549a2a8629445b6faee0848547
CleanOak <53487230+CleanOak@users.noreply.github.com>
2023-12-04 12:08:20 +0000

    Add delete button to clear task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,23 @@
 const inputBox = document.getElementById("myInput");
-let itemsList = document.getElementsByTagName("content");
+let itemsList = document.getElementById("content-list");
 
 
 function addTask(){
-
+    //conditional statment to alert empty input box and add task
     if (inputBox.value === ''){
         alert("You must add a task!")
     } else {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         itemsList.appendChild(li);
+
+        //create 'x' button to delete added task
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span)
     }
+    //empty input box after adding task
+    inputBox.value ='';
 }
 
 
